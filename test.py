f61ae48448152d81779169a5aed19d20f2696b61
Guoyu Yang <49688633+gyyang23@users.noreply.github.com>
2024-09-05 12:04:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import os
 
 # Single gpu
-# os.system("CUDA_VISIBLE_DEVICES=0 python ./mmsegmentation/tools/test.py rdrnet-simple_2xb6-120k_cityscapes-1024x1024.py ./weight/seg/rdrnet_weight.pth")
+# os.system("CUDA_VISIBLE_DEVICES=0 python ./mmsegmentation/tools/test.py rdrnet-s-simple_2xb6-120k_cityscapes-1024x1024.py ./weight/seg/rdrnet_weight.pth")
